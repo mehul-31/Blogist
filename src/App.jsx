661d8b98cbee38805d61826e554ae9bf0b4c857c
Mehul Nandaniya <220130107062@gecg28.ac.in>
2025-04-29 15:33:11 +0530

    Add Header, Footer, and Container components; implement Logout button and Input field; enhance App component with loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import './loader.css';  // <-- Path depending on where your App.jsx is
+
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -19,19 +21,30 @@ function App() {
 
   return !loading ? 
   (
-  <div className="bg-gray-800 w-screen h-screen ">
-    <h1 className="text-amber-500 text-9xl">Blog App Using AppWrite</h1>
-  </div>
-  ) 
-
-  : 
-
-  (
-    <div>
-      // will show the loading animation
+    <div className="bg-gray-800 w-screen h-screen flex items-center justify-center">
+      <h1 className="text-amber-500 text-5xl md:text-7xl lg:text-9xl font-bold text-center">
+        Blog App Using AppWrite
+      </h1>
     </div>
-  );
+  ) 
+  : 
   
+(
+  <div className="flex items-center justify-center bg-gray-900 w-screen h-screen">
+    <div className="relative w-28 flex justify-between">
+      {/* Left Eye */}
+      <div className="w-12 h-12 bg-white rounded-full overflow-hidden flex items-center justify-center">
+        <div className="w-7 h-7 bg-black rounded-full animate-eyeMove"></div>
+      </div>
+
+      {/* Right Eye */}
+      <div className="w-12 h-12 bg-white rounded-full overflow-hidden flex items-center justify-center">
+        <div className="w-7 h-7 bg-black rounded-full animate-eyeMove"></div>
+      </div>
+    </div>
+  </div>
+)
+
 }
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import Container from "./Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
     Footer,
+    Container,
+    Logo,
+    LogoutBtn,
 }

c082fab7ad3e63a564161d89fddb924546134c7e
Mehul Nandaniya <220130107062@gecg28.ac.in>
2025-04-30 21:10:07 +0530

    Add Signup, Login, and PostForm components; implement RTE for rich text editing in PostForm; create AddPost page

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,9 @@ import { Header } from "./Header/Header";
 import Container from "./Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "../components/Signup";
+import Login from "./Login";
+import PostForm from "./post-from/PostForm";
 
 export {
     Header,
@@ -10,4 +13,8 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    Signup,
+    Login,
+    PostForm,
+    
 }
